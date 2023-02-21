2f12d81d7973455d0cc47f66aff409ec8adbdcb5
rafiki1990 <blaiserafiki84@gmail.com>
2023-02-21 14:01:22 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - A program of main function
+ * _putchar: A program that prints _putchar in letter form
+ * Return: always 0
+ */
+
+
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+	return (0);
+}
+
