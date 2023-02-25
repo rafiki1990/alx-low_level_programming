0bcee59b7bd48c1abe6608603d9e8012a60eb943
rafiki1990 <blaiserafiki84@gmail.com>
2023-02-24 16:17:20 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all possible different combinations of two digits
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < 10; n1++)
+	{
+		for (n2 = n1 + 1; n2 < 10; n2++)
+		{
+			putchar((n1 % 10) + '0');
+			putchar((n2 % 10) + '0');
+
+			if (n1 == 8 && n2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
