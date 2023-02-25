c7551008e08bb2371f5a9d54cc32113ee276f4ad
rafiki1990 <blaiserafiki84@gmail.com>
2023-02-24 17:43:32 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 10 times
+ * Return: number
+ */
+void more_numbers(void)
+{
+	int c, d;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (d = 0; d <= 14; d++)
+		{
+			if (d > 9)
+			{
+			_putchar((d / 10) + '0');
+			}
+			_putchar((d % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
