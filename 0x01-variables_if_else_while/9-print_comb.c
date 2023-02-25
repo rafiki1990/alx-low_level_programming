910bf1fcd08b42a055d9f092a28d6619f20af4d6
rafiki1990 <blaiserafiki84@gmail.com>
2023-02-24 16:14:19 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints number in ascending order
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar((n % 10) + '0');
+	if (n == 9)
+		continue;
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
