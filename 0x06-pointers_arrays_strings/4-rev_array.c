d2da1056bc6b71453889043ad66c6f508035cab1
rafiki1990 <blaiserafiki84@gmail.com>
2023-03-01 09:13:30 -0800

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,15 +1,20 @@
 #include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+* reverse_array - reverse array of integers
+* @a: array
+* @n: number of elements of array
+*
+* Return: void
 
-	print_array(a, sizeof(a) / sizeof(int));
-	reverse_array(a, sizeof(a) / sizeof(int));
-	print_array(a, sizeof(a) / sizeof(int));
-	return (0);
+*/
+void reverse_array(int *a, int n)
+{
+int i;
+int t;
+for (i = 0; i < n--; i++)
+{
+t = a[i];
+a[i] = a[n];
+a[n] = t;
+}
 }
