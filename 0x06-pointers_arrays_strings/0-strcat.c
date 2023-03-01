a3ebe254360bc4cc435c17c83766cb9c6d1e7d7d
rafiki1990 <blaiserafiki84@gmail.com>
2023-03-01 08:12:52 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-#icnlude "main.h"
+#include "main.h"
 
 /**
  * _strcat - concatenates two strings
@@ -12,17 +12,17 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
-	while (dest[i] != '/0')
+	while (dest[i] != '\0')
 	{
 		i++
 	}
 	j = 0;
-	while (src[j] != '/0')
+	while (src[j] != '\0')
 	{
 		dest[i] src[j];
 		i++;
 		j++;
 	}
-	dest[i] = '/0';
+	dest[i] = '\0';
 	return (dest);
 }
