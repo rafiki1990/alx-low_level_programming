dc2defae4f0b3f7d30436064ce319a66eef530b3
rafiki1990 <blaiserafiki84@gmail.com>
2023-03-08 16:36:25 -0800

    100-is-palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -15,7 +15,7 @@ int is_palindrome(char *s)
 	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 /**
- * _strlen_recursion - returns the length of a string 
+ * _strlen_recursion - returns the length of a string
  * @s: string to calculate the length of
  *
  * Return: length of the string
