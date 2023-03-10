ee4de62db7741eb44ba9b7394cd6b37919395393
rafiki1990 <blaiserafiki84@gmail.com>
2023-03-10 05:23:01 -0800

    0-whatsmyname.c

--- 0-whatsmyname.c
+++ 0-whatsmyname.c
@@ -1,10 +1,11 @@
 #include "main.h"
-/**main - prints the name of the progrma
+/**
+ * main - prints the name of the progrma
  * @argc: nnmber of argurements
  * @argv: aray of argurements
  * Return: Always 0 (Success)
  */
-int main(int argc_attribut_((umused)), char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
