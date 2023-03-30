5cb679580c35e62a2d973117848e7361b0a20685
rafiki1990 <rafikiblaise@gmail.com>
2023-03-30 15:31:25 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,25 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef LINKED
+#define LINKED
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * str: string-(mallo'ed string)
+ * len: length of the string
+ * next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct list_
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+}list_t;
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
