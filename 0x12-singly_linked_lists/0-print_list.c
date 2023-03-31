c42b4cf0e99da8bc3c86a9693c02b52da26664c3
rafiki1990 <rafikiblaise@gmail.com>
2023-03-31 09:40:11 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t s = 0;
+size_t node_count = 0;
 
-while (h)
+while (h != NULL)
 {
-if (!h->str)
-printf("[0] (nil)\n");
+if (h->str == NULL)
+	printf("[0] (nil)\n");
 else
-printf("[%u] %s\n", h->len, h->str);
+	printf("[%d] %s\n", h->len, h->str);
+node_count++;
 h = h->next;
-s++;
+
 }
 
-return (s);
+return (node_count);
 }
