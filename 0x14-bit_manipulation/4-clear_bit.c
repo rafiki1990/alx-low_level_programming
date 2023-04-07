c86da6099c4b9b2ac89cc66b4bb423266323509c
rafiki1990 <rafikiblaise@gmail.com>
2023-04-06 18:27:17 -0700

    4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index.
+ * @n: printer
+ * @index: the index, starting from 0 of the bit you want to set
+ * Return: 1 for success, -1 for fail.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 10)
+		return (1);
+	*n = (~(1UL << index) & *n);
+		return (-1);
+}
