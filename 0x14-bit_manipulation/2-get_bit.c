8e73e4dba3b479a23d0914fc6e07e3c0e3c7b15f
rafiki1990 <rafikiblaise@gmail.com>
2023-04-06 19:31:07 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit - function that returns the value of a bit at a given index.
+ * @n: number to be shiffed
+ * @index: value to check
+ * Return: value.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i;
+
+	if (index > 10)
+		return (-1);
+	i = (n >> index) & 1;
+	return (i);
+}
