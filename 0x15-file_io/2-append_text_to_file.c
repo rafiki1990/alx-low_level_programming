23d3446c61da06e314a770fa81b65c20281ab429
rafiki1990 <rafikiblaise@gmail.com>
2023-04-11 15:07:32 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, i);
 
-	if (o == -1 || w == -1)
+	if (o == -1 || w == 1)
 	return (-1);
 
 	close(o);
