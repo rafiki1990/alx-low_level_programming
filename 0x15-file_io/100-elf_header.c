dd4e060f0606477defbbd68aa11b5ac49cf961f4
Rafiki Blaise <rafikiblaise@gmail.com>
2023-07-15 05:57:48 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -6,16 +6,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void check_elf(unsigned char *e_ident);
+void check_elf(int elf, unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
 void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_header2(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf);
 
 /**
  * check_elf - Checks if a file is an ELF file.
@@ -244,10 +240,8 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	check_elf(header->e_ident);
 	printf("ELF Header:\n");
 	print_magic(header->e_ident);
-	print_class(header->e_ident);
 	print_data(header->e_ident);
-	print_version(header->e_ident);
-	print_header1(header->e_ident);
+	print_osabi(header->e_ident);
 	print_header2(header->e_ident);
 	print_type(header->e_type, header->e_ident);
 	print_entry(header->e_entry, header->e_ident);
