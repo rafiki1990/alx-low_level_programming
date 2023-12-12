83234b4c3409b677dc510d7a71025dcc454969cd
Rafiki Blaise <rafikiblaise@gmail.com>
2023-12-12 02:42:44 -0800

    Search Algorithms mondatory done

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,18 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include<stdio.h>
+
+/* Function prototype for linear_search */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+int advanced_binary_recursive(int *array, size_t left,
+		size_t right, int value);
+int my_binary_search(int *array, size_t size, int value);
+
+
+
+#endif /* SEARCH_ALGOS_H */
